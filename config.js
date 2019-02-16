be025119f2dbff7c13de88fa25d39b6e64590b92
Alessandro <4512372+Ale32bit@users.noreply.github.com>
2019-02-16 19:23:58 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: 2998, // Port to listen
+    url: "https://kristium.ale32bit.me/", // Include ending slash
+    kristNode: "http://krist.ceriat.net/",
+};
---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const app = express();
+const expressWs = require("express-ws")(app);
+const request = require("snekfetch");
+const config = (() => {
+    try {
+        return require("./config.js");
+    } catch (e) {
+        console.error("")
+    }
+})();
+
+const charset = /^[a-zA-Z0-9\-.]*$/;
+
+const servers = {};
+const reqs = {};
+
+function genID(len = 64){
+    let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let id = "";
+    for(let i = 0; i<len; i++){
+        id += charset[Math.floor(Math.random()*charset.length)];
+    }
+    return id
+}
+
+
+
+function getA(address) {
+    address = address.replace(/\.kst$/, "");
+    return new Promise((resolve, reject) => {
+        request.get(config.kristNode+"names/" + address)
+            .then(r => {
+                let b = r.body;
+                if (b.ok) {
+                    if (b.name.a !== "") {
+                        return resolve(b.name.a);
+                    } else {
+                        return reject("IP not found")
+                    }
+                } else {
+                    return reject("Name not found")
+                }
+            })
+            .catch(console.error)
+    })
+}
+
+app.ws("/", function (ws, req) {
+    ws.ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(/\s*,\s*/)[0] : req.connection.remoteAddress;
+    ws.ip = ws.ip.replace(/^.*:/, '');
+    servers[ws.ip] = ws;
+    console.log("New connection", ws.ip);
+    let pingInt = setInterval(()=>{
+        ws.send(JSON.stringify({
+            type: "ping",
+        }))
+    }, 10*1000);
+    ws.on('message', function (data) {
+        try {
+            data = JSON.parse(data);
+        } catch (e) {
+            data = undefined;
+            ws.send(JSON.stringify({
+                type: "error",
+                error: e.toString(),
+                ok: false,
+            }))
+        }
+        if (!data) return false;
+        console.log(data);
+        switch(data.type){
+            case "request":
+                if(data.requestID && reqs[data.requestID]){
+                    let req = reqs[data.requestID].req;
+                    let res = reqs[data.requestID].res;
+
+                    res.end(data.body);
+                    delete reqs[data.requestID];
+                }
+                break;
+        }
+    });
+
+    ws.on("close", function(){
+        clearInterval(pingInt)
+    })
+});
+
+app.use(express.static("public"));
+
+let provider = new express.Router();
+
+provider.get("*", function(req,res, next){
+    let domain = req.baseUrl;
+    domain = domain.replace(/^\//,"");
+    console.log("GET", domain);
+    getA(domain).then(ip => {
+        if (servers[ip]) {
+            let reqID =  genID();
+            reqs[reqID] = {
+                req: req,
+                res: res,
+            };
+            servers[ip].send(JSON.stringify({
+                type: "request",
+                method: "GET",
+                name: domain,
+                path: req.url,
+                requestID: reqID,
+            }))
+        }
+    })
+});
+
+app.use("/:domain.kst/",provider);
+
+app.listen(config.port, () => console.log("Listening"));
